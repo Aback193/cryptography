c52f76f3b8ada58bcb8ebdc9e2ce78b35794a4bb
Uros Milivojevic <milivojevicu1998@gmail.com>
2022-01-22 13:44:43 +0100

    Izmenjena ocr.py skripta
    Skripta sada prima argumente preko kojih se mogu proslediti putanja do
    ulaznog PDF fajla i putanja na koju treba biti upisan izlazni TXT fajl.

--- ocr.py
+++ ocr.py
@@ -1,34 +1,64 @@
+"""
+Skripta moze da se pokrene sa sledećim argumentima:
+1. (obavezan) Put do PDF fajla nad kome treba uraditi OCR.
+2. (opcioni) Put do TXT fajla u koji treba da se upise rezultat OCR-a.
+    Ako ovaj argument nije naveden, rezultat će biti ispisan na stadardni output,
+    tako da rezultat moze da se dobije u C# programu umesto da se odmah upise u
+    fajl ukoliko ima potrebe za tim.
+
+Iz C# se moze pokrenuti sa argumentima uz pomoć ProcessStartInfo.Arguments.
+"""
+
 from pdf2image import convert_from_path
-from pytesseract import image_to_string     # pytesseract must be installed systemwide aswell
-import cv2      # pip install opencv-python
+# tesseract must be installed systemwide aswell
+# pip install pytesseract
+from pytesseract import image_to_string
+import cv2  # pip install opencv-python
+import sys
 import os
 
-imgPathTemp = 'out.jpg'
 
-def save_txt(text, fileName):
-    text_file = open(fileName, "w")
-    text_file.write(text)
-    text_file.close()
+TMP_IMG = 'tmp.jpg'
 
-def convert_pdf_to_img(pdf_file):
-    return convert_from_path(pdf_file)
 
 def convert_image_to_text(file):
     img = cv2.imread(file)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     text = image_to_string(img, config='--psm 6').strip()
+
     return text
+
 
 def get_text_from_any_pdf(pdf_file):
     print("Performing OCR, please wait...")
-    images = convert_pdf_to_img(pdf_file)
+    images = convert_from_path(pdf_file)
     final_text = ""
+
     for pg, img in enumerate(images):
-        img.save(imgPathTemp, 'JPEG')
-        final_text += convert_image_to_text(imgPathTemp)
+        img.save(TMP_IMG, 'JPEG')
+        final_text += convert_image_to_text(TMP_IMG)
+    os.remove(TMP_IMG)
+
     return final_text
 
-path_to_pdf = 'file.pdf'
-ocrText = get_text_from_any_pdf(path_to_pdf)
-save_txt(ocrText, "text_ocr-{}".format(path_to_pdf))
-os.remove(imgPathTemp)
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)  # nije prosledjen ni jedan argument
+
+    path_to_pdf = sys.argv[1]
+    path_to_txt = None
+    if len(sys.argv) > 2:
+        path_to_txt = sys.argv[2]
+
+    ocr_text = get_text_from_any_pdf(path_to_pdf)
+
+    if path_to_txt is not None:
+        with open(path_to_txt, "w") as text_file:
+            text_file.write(ocr_text)
+    else:
+        print(ocr_text)
+
+
+if __name__ == '__main__':
+    main()
