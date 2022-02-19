626b0b38474509d1b437c30bc3eec3ab977c07a3
Jovan Pesic <enimen@elfak.rs>
2022-02-19 20:46:18 +0100

    OCR PDf

--- DocumentFinder/MainWindow.xaml.cs
+++ DocumentFinder/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Office.Interop.Word;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Xceed.Words.NET;      // https://www.c-sharpcorner.com/article/generate-word-document-using-c-sharp/
 using Application = Microsoft.Office.Interop.Word.Application;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using Tesseract;
 
 namespace DocumentFinder
 {
@@ -213,6 +217,62 @@ namespace DocumentFinder
             }            
         }
 
+
+        public string ExtractTextFromPdfWithOCR(string pdfFile)
+        {
+            StringBuilder text = new StringBuilder();
+            try
+            {
+                using (var engine = new TesseractEngine(@"./testdata", "eng", EngineMode.Default))
+                {
+                    using (iTextSharp.text.pdf.PdfReader pdf = new PdfReader(pdfFile))
+                    {
+
+                        for (int pageNumber = 1; pageNumber <= pdf.NumberOfPages; pageNumber++)
+                        {
+                            text.Append(PdfTextExtractor.GetTextFromPage(pdf, pageNumber));
+                            PdfDictionary pg = pdf.GetPageN(pageNumber);
+
+                            IList<System.Drawing.Image> listImages = GetImagesFromPdfDict(pg, pdf);
+                            if (listImages == null)
+                            {
+                                continue;
+                            }
+                            var imageNumber = 1;
+                            foreach (var obj in listImages)
+                            {
+
+                                try
+                                {
+                                    var bmp = new Bitmap(obj);
+                                    var img = PixConverter.ToPix(bmp);
+                                    using (var image = engine.Process(img))
+                                    {
+                                        var textFromImage = image.GetText();
+
+                                        text.Append(textFromImage);
+
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Trace.WriteLine("Execption for image on page:" + pageNumber + "Image Number:" + imageNumber);
+                                }
+                                imageNumber++;
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Trace.WriteLine("PDF to Text Exception");
+            }
+
+            return text.ToString();
+        }
+
         // DOCX to Text with DocX library
         public string ExtractTextFromDocX(string filePath)
         {
@@ -290,7 +350,7 @@ namespace DocumentFinder
                 {
                     Trace.WriteLine("pdfConversionFilePaths: " + pdfConversionFilePaths[i].ToString());
 
-                    string pdfText = ExtractTextFromPdf(pdfConversionFilePaths[i]);
+                    string pdfText = ocrOption.IsChecked == true ? ExtractTextFromPdfWithOCR(pdfConversionFilePaths[i]) : ExtractTextFromPdf(pdfConversionFilePaths[i]);
                     string pdfToTextFile = pdfConversionFilePaths[i] + ".txt";
                     if (pdfText != "")
                     {
@@ -454,6 +514,45 @@ namespace DocumentFinder
             {
                 MessageBox.Show("No files were coppied yet! Please click on " + btnFind.Content);
             }
+        }
+
+
+        private IList<System.Drawing.Image> GetImagesFromPdfDict(PdfDictionary dict, PdfReader doc)
+        {
+            List<System.Drawing.Image> images = new List<System.Drawing.Image>();
+            PdfDictionary res = (PdfDictionary)(PdfReader.GetPdfObject(dict.Get(PdfName.RESOURCES)));
+            PdfDictionary xobj = (PdfDictionary)(PdfReader.GetPdfObject(res.Get(PdfName.XOBJECT)));
+
+            if (xobj != null)
+            {
+                foreach (PdfName name in xobj.Keys)
+                {
+                    PdfObject obj = xobj.Get(name);
+                    if (obj.IsIndirect())
+                    {
+                        PdfDictionary tg = (PdfDictionary)(PdfReader.GetPdfObject(obj));
+                        PdfName subtype = (PdfName)(PdfReader.GetPdfObject(tg.Get(PdfName.SUBTYPE)));
+                        if (PdfName.IMAGE.Equals(subtype))
+                        {
+                            int xrefIdx = ((PRIndirectReference)obj).Number;
+                            PdfObject pdfObj = doc.GetPdfObject(xrefIdx);
+                            PdfStream str = (PdfStream)(pdfObj);
+
+                            iTextSharp.text.pdf.parser.PdfImageObject pdfImage =
+                                new iTextSharp.text.pdf.parser.PdfImageObject((PRStream)str);
+                            System.Drawing.Image img = pdfImage.GetDrawingImage();
+
+                            images.Add(img);
+                        }
+                        else if (PdfName.FORM.Equals(subtype) || PdfName.GROUP.Equals(subtype))
+                        {
+                            images.AddRange(GetImagesFromPdfDict(tg, doc));
+                        }
+                    }
+                }
+            }
+
+            return images;
         }
     }
 }
